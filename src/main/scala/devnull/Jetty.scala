72287b87d3855afabe674738adb89b42b818087d
Henrik Nordvik <henrikno@gmail.com>
2015-02-24 23:20:46 +0100

    Add access-log.

--- src/main/scala/devnull/Jetty.scala
+++ src/main/scala/devnull/Jetty.scala
@@ -7,7 +7,7 @@ object Jetty extends App {
 
   private val server = unfiltered.jetty.Server.http(port).context(contextPath) {
     _.plan(Resources())
-  }
+  }.requestLogging("access.log")
 
   server.underlying.setSendDateHeader(true)
   server.run( _ => println("Running server at " + port))
