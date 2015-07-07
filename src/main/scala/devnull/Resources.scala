017e81a3089cda36f2be0edede6e889855af0321
Ingar Abrahamsen <ingar@abrahams1.com>
2015-07-07 23:43:07 +0200

    Replaces ping with json response

--- src/main/scala/devnull/Resources.scala
+++ src/main/scala/devnull/Resources.scala
@@ -22,6 +22,8 @@ import scalaz.concurrent.Task
 class Resources(val feedbackRepository: FeedbackRepository, xa: Transactor[Task]) extends Plan {
   type ResponseDirective = Directive[HttpServletRequest, ResponseFunction[Any], ResponseFunction[Any]]
 
+  case class Ping(ping: String)
+
   override def intent: Intent = Intent {
     case Root() => handleRoot()
     case Links.Feedbacks(eventId, sessionId) => handleFeedbacks(eventId, sessionId)
@@ -32,7 +34,7 @@ class Resources(val feedbackRepository: FeedbackRepository, xa: Transactor[Task]
     val get = for {
       _ <- GET
     } yield {
-      JsonContent ~> ResponseString( """{"ping":"ok"} """)
+      Ok ~> ResponseJson(Ping("pong"))
     }
     get
   }
