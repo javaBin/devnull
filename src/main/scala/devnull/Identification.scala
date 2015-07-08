d0d7b427c0031ccb4d6c5bafdf5a1970f546c5f7
Ingar Abrahamsen <ingar@abrahams1.com>
2015-07-08 20:14:28 +0200

    Adding userAgent to VoterInfo

--- src/main/scala/devnull/Identification.scala
+++ src/main/scala/devnull/Identification.scala
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest
 
 import devnull.storage.VoterInfo
 import unfiltered.directives.Directives._
-import unfiltered.request.StringHeader
-import unfiltered.response.BadRequest
+import unfiltered.directives.FilterDirective
+import unfiltered.directives.Result.Success
+import unfiltered.request.{UserAgent, HttpRequest, StringHeader}
+import unfiltered.response.{BadRequest, ResponseFunction}
 
 sealed trait Identification
 
@@ -14,7 +16,20 @@ object Identification {
     for {
       voterId <- commit(when { case VoterIdHeader(voterId) => voterId }.orElse(BadRequest))
       ipAddress <- commit(request[HttpServletRequest].map(r => r.remoteAddr))
-    } yield VoterInfo(voterId, ipAddress, "unknown")
+      userAgent <- commit(whenOr { case UserAgent(ua) => ua }.orElse("unknown"))
+    } yield VoterInfo(voterId, ipAddress, userAgent)
+
+
+  /* HttpRequest has to be of type Any because of type-inference (SLS 8.5) */
+  case class whenOr[A](f:PartialFunction[HttpRequest[Any], A]){
+    def orElse[R](elseValue: A) =
+      new FilterDirective[Any, ResponseFunction[R], A](r =>
+        if(f.isDefinedAt(r)) Success(f(r)) else Success(elseValue),
+        _ =>  Success(elseValue)
+      )
+  }
+
+
 }
 
 private object VoterIdHeader extends StringHeader("Voter-ID")
