ee5e234f1c58cf1d96cd9ba6f0e9b4edb3c61242
Ingar Abrahamsen <ingar@abrahams1.com>
2015-07-08 19:58:30 +0200

    Added ip address to voterInfo

--- src/main/scala/devnull/Identification.scala
+++ src/main/scala/devnull/Identification.scala
@@ -1,18 +1,20 @@
 package devnull
 
+import javax.servlet.http.HttpServletRequest
+
+import devnull.storage.VoterInfo
 import unfiltered.directives.Directives._
 import unfiltered.request.StringHeader
 import unfiltered.response.BadRequest
 
 sealed trait Identification
 
-case class VoterId(deviceId: String) extends Identification
-
 object Identification {
   def identify() =
     for {
       voterId <- commit(when { case VoterIdHeader(voterId) => voterId }.orElse(BadRequest))
-    } yield VoterId(voterId)
+      ipAddress <- commit(request[HttpServletRequest].map(r => r.remoteAddr))
+    } yield VoterInfo(voterId, ipAddress, "unknown")
 }
 
 private object VoterIdHeader extends StringHeader("Voter-ID")
