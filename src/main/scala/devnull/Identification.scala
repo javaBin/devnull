c2162448543af471dd2c54c3bc1bd5b6eab3b221
Ingar Abrahamsen <ingar@abrahams1.com>
2015-07-11 08:36:52 +0200

    using forwarded-for header before the ip address
    We're running behind a nginx proxy so we need to fetch the
    origin ip address from the http header if it's provided.

--- src/main/scala/devnull/Identification.scala
+++ src/main/scala/devnull/Identification.scala
@@ -6,7 +6,7 @@ import devnull.storage.VoterInfo
 import unfiltered.directives.Directives._
 import unfiltered.directives.FilterDirective
 import unfiltered.directives.Result.Success
-import unfiltered.request.{UserAgent, HttpRequest, StringHeader}
+import unfiltered.request.{HttpRequest, StringHeader, UserAgent, XForwardedFor}
 import unfiltered.response.{BadRequest, ResponseFunction}
 
 sealed trait Identification
@@ -15,17 +15,24 @@ object Identification {
   def identify() =
     for {
       voterId <- commit(when { case VoterIdHeader(voterId) => voterId }.orElse(BadRequest))
-      ipAddress <- commit(request[HttpServletRequest].map(r => r.remoteAddr))
       userAgent <- commit(whenOr { case UserAgent(ua) => ua }.orElse("unknown"))
-    } yield VoterInfo(voterId, ipAddress, userAgent)
+      ipAddress <- commit(request[HttpServletRequest].map(r => r.remoteAddr))
+      forwardFor <- commit(whenOr { case XForwardedFor(ff) => headOfList(ff) }.orElse(None))
+    } yield VoterInfo(voterId, forwardFor.getOrElse(ipAddress), userAgent)
 
+  private def headOfList(ff: List[String]): Option[String] = {
+    ff match {
+      case x :: xs => Some(x)
+      case Nil => None
+    }
+  }
 
   /* HttpRequest has to be of type Any because of type-inference (SLS 8.5) */
-  case class whenOr[A](f:PartialFunction[HttpRequest[Any], A]){
+  case class whenOr[A](f: PartialFunction[HttpRequest[Any], A]) {
     def orElse[R](elseValue: A) =
       new FilterDirective[Any, ResponseFunction[R], A](r =>
-        if(f.isDefinedAt(r)) Success(f(r)) else Success(elseValue),
-        _ =>  Success(elseValue)
+        if (f.isDefinedAt(r)) Success(f(r)) else Success(elseValue),
+        _ => Success(elseValue)
       )
   }
 
