77a0236d74960cfd54aebd7593cd7c989de7645c
Ingar Abrahamsen <ingar@abrahams1.com>
2015-08-27 00:11:49 +0200

    Added app-info resource.
    scala objects can't be serialized to json stings for some reasons. Tried to google it
    but I didn't find any good answers. Since classes worked fine I just wrapped the
    object in a case class.

---
+++ src/main/scala/devnull/rest/AppInfo.scala
@@ -0,0 +1,38 @@
+package devnull.rest
+
+import java.util.Date
+import javax.servlet.http.HttpServletRequest
+
+import devnull.rest.helpers.ResponseWrites.ResponseJson
+import unfiltered.directives.Directive
+import unfiltered.directives.Directives._
+import unfiltered.request.GET
+import unfiltered.response.{Ok, ResponseFunction}
+
+object AppInfo {
+  type ResponseDirective = Directive[HttpServletRequest, ResponseFunction[Any], ResponseFunction[Any]]
+
+  def handelAppInfo(): ResponseDirective = {
+    val get = for {
+      _ <- GET
+    } yield {
+        Ok ~> ResponseJson(AppInfoContent(
+          devnull.BuildInfo.scalaVersion,
+          devnull.BuildInfo.version,
+          new Date(devnull.BuildInfo.buildTime),
+          devnull.BuildInfo.branch,
+          devnull.BuildInfo.sha
+        ))
+      }
+
+    get
+  }
+
+  case class AppInfoContent(
+      scalaVersion: String,
+      version: String,
+      buildTime: Date,
+      branch: String,
+      sha: String)
+
+}
