5cbf73e1fb9357d63fb3c7254a233af5d61bf9ee
Ingar Abrahamsen <ingar@abrahams1.com>
2015-08-28 01:09:27 +0200

    Http client wrapper
    Makes integration with CollectionJson easier.

---
+++ src/main/scala/devnull/ems/HttpClient.scala
@@ -0,0 +1,28 @@
+package devnull.ems
+
+import com.ning.http.client
+import dispatch.{url => reqUrl, _}
+import net.hamnaberg.json.collection.JsonCollection
+
+trait HttpClient {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val c: Http = new Http().configure(_.setFollowRedirects(true))
+
+  def baseUrl: String
+
+  def request(pathElements: List[String]): Future[JsonCollection] = {
+    val req: Req = pathElements.reverse.foldRight(reqUrl(baseUrl))((path, re) => re / path)
+    print(req.toRequest.getRawUrl)
+    c(req OK Collection)
+  }
+
+}
+
+object Collection extends (client.Response => JsonCollection) {
+  override def apply(r: client.Response): JsonCollection = {
+    val parsed: Either[Throwable, JsonCollection] = JsonCollection.parse(r.getResponseBodyAsStream)
+    parsed.right.get
+  }
+}
