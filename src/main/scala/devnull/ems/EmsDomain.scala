78e46a5513790ea00d8d374399242f2173605a2e
Ingar Abrahamsen <ingar@abrahams1.com>
2015-08-28 02:15:49 +0200

    Added apply method for EventId and SessionId
    Makes life easier :)

--- src/main/scala/devnull/ems/EmsDomain.scala
+++ src/main/scala/devnull/ems/EmsDomain.scala
@@ -5,13 +5,18 @@ import java.util.UUID
 
 sealed trait Id {
   def id: UUID
-
   override def toString: String = id.toString
 }
 
 case class EventId(id: UUID) extends Id
+object EventId {
+  def apply(id: String): EventId = new EventId(UUID.fromString(id))
+}
 
 case class SessionId(id: UUID) extends Id
+object SessionId {
+  def apply(id: String): SessionId = new SessionId(UUID.fromString(id))
+}
 
 case class Session(
     eventId: EventId,
