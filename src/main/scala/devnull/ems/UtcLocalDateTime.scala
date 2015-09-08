9931102bc3caf9567188ee2b717b69b2137f1edf
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-09-08 18:16:49 +0200

    Use specific clock. open 10 minutes BEFORE the session ends.

--- src/main/scala/devnull/ems/UtcLocalDateTime.scala
+++ src/main/scala/devnull/ems/UtcLocalDateTime.scala
@@ -1,10 +1,10 @@
 package devnull.ems
 
-import java.time.{Instant, LocalDateTime, ZoneOffset}
+import java.time.{Clock, Instant, LocalDateTime, ZoneOffset}
 
 object UtcLocalDateTime {
 
-  def now(): LocalDateTime = LocalDateTime.now(ZoneOffset.UTC)
+  def now()(implicit clock: Clock): LocalDateTime = LocalDateTime.now(clock)
 
   def parse(utcTime: String):LocalDateTime  = LocalDateTime.ofInstant(Instant.parse(utcTime), ZoneOffset.UTC)
 
