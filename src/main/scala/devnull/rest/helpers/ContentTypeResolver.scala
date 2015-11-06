ee1f3600904921e2a72afef216a142d81ebbb8a8
Ingar Abrahamsen <ingar@abrahams1.com>
2015-11-06 12:44:21 +0200

    Removed unused code
    A lot of the code was copied from unfiltered to fix
    the charset that firefox added to content type. This is
    refactored to use mime type so we don't need this
    code anymore.

--- src/main/scala/devnull/rest/helpers/ContentTypeResolver.scala
+++ src/main/scala/devnull/rest/helpers/ContentTypeResolver.scala
@@ -1,28 +1,12 @@
 package devnull.rest.helpers
 
 import devnull.rest.MIMEType
-import devnull.rest.helpers.DirectiveHelper.{some, when}
+import devnull.rest.helpers.DirectiveHelper.when
 import unfiltered.directives.Directives.commit
-import unfiltered.request.{HttpRequest, RequestContentType => URequestContentType, RequestExtractor}
+import unfiltered.request.{HttpRequest, RequestExtractor}
 import unfiltered.response.UnsupportedMediaType
 
 object ContentTypeResolver {
-
-  def withContentType(ct: String) = commit(when {
-    case URequestContentType(`ct`) => ct
-  }.orElse(UnsupportedMediaType))
-
-  def validContentType = commit(
-    some{
-      val json: PartialFunction[HttpRequest[Any], SupportedContentType] = {
-        case CharsetRequestContentType(JsonContentType.value) => JsonContentType
-      }
-      val jsonCollection: PartialFunction[HttpRequest[Any], SupportedContentType] = {
-        case CharsetRequestContentType(CollectionJsonContentType.value) => CollectionJsonContentType
-      }
-      List(json, jsonCollection
-    ) }.orElse(UnsupportedMediaType)
-  )
 
   def withContentTypes(ct: List[MIMEType]) = commit(when {
     case RequestContentType(contentType) if ct.exists(m => m.includes(contentType)) => contentType
@@ -34,22 +18,3 @@ object RequestContentType extends RequestExtractor[MIMEType] {
   override def unapply[T](req: HttpRequest[T]): Option[MIMEType] =
     req.headers("Content-Type").toList.headOption.flatMap(MIMEType.apply)
 }
-
-object CharsetRequestContentType extends RequestHeader("Content-Type")(StringValueParser)
-
-class RequestHeader[A](val name: String)(parser: Iterator[String] => List[A]) extends RequestExtractor[A] {
-  def unapply[T](req: HttpRequest[T]) = parser(req.headers(name)).headOption
-  def apply[T](req: HttpRequest[T]) = parser(req.headers(name)).headOption
-}
-
-object StringValueParser extends (Iterator[String] => List[String]) {
-  def apply(values: Iterator[String]) =
-    values.toList.map(v => v.replaceAll("; charset=.*", ""))
-}
-
-sealed abstract class SupportedContentType(val value: String) {
-  override def toString: String = value
-}
-
-object JsonContentType extends SupportedContentType("application/json")
-object CollectionJsonContentType extends SupportedContentType("application/vnd.collection+json")
