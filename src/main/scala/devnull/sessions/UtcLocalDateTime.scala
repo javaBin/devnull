0fd6e290523161875e21a82e3c1fa0a7f644f44e
Ingar Abrahamsen <ingar@abrahams1.com>
2017-07-25 15:24:51 +0200

    test: add test for UtcLocalDateTime

--- src/main/scala/devnull/sessions/UtcLocalDateTime.scala
+++ src/main/scala/devnull/sessions/UtcLocalDateTime.scala
@@ -4,9 +4,11 @@ import java.time.{Clock, Instant, LocalDateTime, ZoneOffset}
 
 object UtcLocalDateTime {
 
-  def now()(implicit clock: Clock): LocalDateTime = LocalDateTime.now(clock)
+  def now()(implicit clock: Clock): LocalDateTime =
+    LocalDateTime.now(clock)
 
-  def parse(utcTime: String):LocalDateTime  = LocalDateTime.ofInstant(Instant.parse(utcTime), ZoneOffset.UTC)
+  def parse(utcTime: String): LocalDateTime =
+    LocalDateTime.ofInstant(Instant.parse(utcTime), ZoneOffset.UTC)
 
 }
 
