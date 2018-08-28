f7b57e1ab6e0adfc229fe27a5163c8b6e3c2f808
Ingar Abrahamsen <ingar@abrahams1.com>
2018-08-28 18:25:40 +0200

    chore: update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
   private lazy val unfilteredVersion = "0.9.1"
 
   val joda = Seq(
-    "joda-time" % "joda-time" % "2.9.9",
-    "org.joda" % "joda-convert" % "1.9.2"
+    "joda-time" % "joda-time" % "2.10",
+    "org.joda" % "joda-convert" % "2.1.1"
   )
 
   val unfiltered = Seq(
@@ -22,13 +22,13 @@ object Dependencies {
     "no.arktekk"              %% "linx"                  % "0.4",
     "io.mth"                  %% "unfiltered-cors"       % "0.4-IA1" exclude("net.databinder", "*"),
     "org.dispatchhttp"        %% "dispatch-core"         % "0.14.0",
-    "org.json4s"              %% "json4s-native"         % "3.5.3"
+    "org.json4s"              %% "json4s-native"         % "3.5.4"
   )
 
   val logging = Seq(
     "org.slf4j"                  %  "slf4j-api"             % "1.7.25",
     "org.slf4j"                  %  "slf4j-simple"          % "1.7.25",
-    "com.typesafe.scala-logging" %% "scala-logging"         % "3.7.2"
+    "com.typesafe.scala-logging" %% "scala-logging"         % "3.9.0"
   )
 
   lazy val doobieVersion = "0.4.4"
@@ -39,10 +39,10 @@ object Dependencies {
     "org.tpolecat"   %% "doobie-postgres"  % doobieVersion  withSources()  exclude("postgresql", "postgresql"),
     "org.tpolecat"   %% "doobie-hikari"    % doobieVersion  withSources(),
     "org.tpolecat"   %% "doobie-scalatest" % doobieVersion  withSources(),
-    "com.zaxxer"     %  "HikariCP"         % "2.7.6"
+    "com.zaxxer"     %  "HikariCP"         % "3.2.0"
   )
 
-  val caffine =    "com.github.ben-manes.caffeine" %  "caffeine"              % "2.6.1"
-  val scalaTest =   "org.scalatest"                %%  "scalatest"            % "3.0.4"   % Test
+  val caffine =    "com.github.ben-manes.caffeine" %  "caffeine"              % "2.6.2"
+  val scalaTest =   "org.scalatest"                %%  "scalatest"            % "3.0.5"   % Test
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,5 @@ addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.9.0")
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.6")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.4")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.4")
